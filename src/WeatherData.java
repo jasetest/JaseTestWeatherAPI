1f85a86ce23790faa76e289cb44f9fb69646ca31
jasetest <jasetest280@gmail.com>
2025-11-12 14:15:23 -0600

    Started WeatherData

---
+++ src/WeatherData.java
@@ -0,0 +1,17 @@
+/*
+
+Weather Data class represents blah blah blah
+
+Example URL: https://open-meteo.com/en/docs?latitude=42.0329&longitude=-97.4229&temperature_unit=fahrenheit&wind_speed_unit=mph&precipitation_unit=inch&timezone=auto&forecast_days=3&current=temperature_2m#location_and_time
+
+ */
+
+
+public class WeatherData {
+    private double temperature; //degrees in fahrenheit
+    private double windSpeed;
+    private int chanceOfRain;
+    private double windDirection;
+    private String city;
+    private String state;
+}
